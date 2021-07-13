e319dee32bcfaaabc7e58fd2e57daa494ec8b985
yingha <yinghalin@gmail.com>
2021-07-13 11:54:20 +0200

    edit setup.py

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import os
+
+def open_file(fname):
+    """helper function to open a local file"""
+    return open(os.path.join(os.path.dirname(__file__), fname))
+
+
+setup(
+    name='artclassifier',
+    version='0.0.1',
+    author='Meng-Ying Lin',
+    author_email='',
+    packages=find_packages(),
+    url='https://github.com/yingha/artclassifier',
+    license='MIT',
+    classifiers=[
+        'Programming Language :: Python :: 3.8',
+    ],
+    package_data= {
+        'artclassifier': ['data/*.csv','data/training/*','models/*.h5']
+    },
+    description='Art style classifier',
+    long_description=open_file('README.md').read(),
+    # end-user dependencies for your library
+    install_requires=[
+        'pandas',
+        'numpy',
+        'tensorflow',
+        'geopy.geocoders',
+        'folium',
+        'python-Levenshtein'
+    ],
+)
